b1c946531ca0694c0d376905d55727f5f0b01687
HasanAbbas2009 <hasanabbaskazim@gmail.com>
2023-12-26 15:43:07 +0530

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float senseX;
+    public float senseY;
+
+    public Transform orientation;
+
+    float Xrotation;
+    float Yrotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
+
+        Yrotation += mouseX;
+        Xrotation -= mouseY;
+        Xrotation = Mathf.Clamp(Xrotation, -90f,  90f);
+
+        transform.rotation = Quaternion.Euler(Xrotation, Yrotation, 0);
+        orientation.rotation = Quaternion.Euler(0, Yrotation, 0);
+
+    }
+}
