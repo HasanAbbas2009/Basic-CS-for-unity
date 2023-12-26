e3d679bd027cc8570e633d5620038dccefb96bc9
HasanAbbas2009 <hasanabbaskazim@gmail.com>
2023-12-26 15:48:04 +0530

    Update Fps_camera_rotation_script.cs

--- Fps_camera_rotation_script.cs
+++ Fps_camera_rotation_script.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+// READ THE README OF THIS SCRIPT FOR INSTRUCTIONS AT FIRST
 
 public class rotation : MonoBehaviour
 {
